4732efd7b27a1e65c7e225501a99425ee197f466
KaikyBrito1 <okaiiky2019@gmail.com>
2026-04-14 20:39:13 -0300

    Add nested loop logic to print numbers
    Implement nested loops to print numbers based on user input.

---
+++ Linguagem C/Sequência Lógica 2 aninhado.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	
+	int X, Y, cont=1;
+	
+	scanf("%d %d", &X, &Y);
+	
+	for(int i = 1; i <= Y;i){
+		for(int j = 1; j <= X;j++ ){
+			if(j==X){
+				printf("%d\n", i);
+			}
+			else{
+				printf("%d ", i);
+				
+			}
+			i++;
+	}
+	}
+	return 0;
+}
