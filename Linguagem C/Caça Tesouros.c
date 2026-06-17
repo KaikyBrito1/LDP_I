c9a5fe90b8e8de753dc0da6880e77ba4996947f3
KaikyBrito1 <okaiiky2019@gmail.com>
2026-06-17 06:33:39 -0300

    Create Caça Tesouros.c

---
+++ Linguagem C/Caça Tesouros.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void apresentarjogo();
+void gerarTesouro(int *x, int *y);
+void lerPosicao (int *x, int *y);
+int verificarTesouro(int jogadorX, int jogadorY, int tesouroX, int tesouroY);
+void darDicas(int jogadorX, int jogadorY, int tesouroX, int tesouroY);
+
+int main(){
+	int tesouroX, tesouroY;
+	int x, y;
+	int tentativas;
+}
